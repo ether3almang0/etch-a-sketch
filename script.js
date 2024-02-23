11e65c79a1239ebb6511f8ce02af33017d1ee914
ether3almang0 <zainab.s.live@gmail.com>
2024-02-23 11:40:54 -0500

    project complete but grid is slighly off

--- script.js
+++ script.js
@@ -1,14 +1,44 @@
 const container = document.getElementById('container');
 
 function makeDivs(numDivs) {
-    for (let d = 0; d < numDivs; d++){
+    for (let d = 0; d < numDivs**2; d++){
         let cells = document.createElement('div');
         container.appendChild(cells);
+        for (let g = 0; g < numDivs**2; g++){
+            if (d == numDivs*g + 1) {
+                cells.setAttribute('class','row');
+                cells.setAttribute('style', 'border: 0; width: 100%; height: 0;');
+            };
+        }
+        cells.classList.add('pixels');
         cells.addEventListener('mouseover', () => {
             cells.style.backgroundColor = "black";
         })
     }
+    //let row = document.querySelectorAll(`.pixels:nth-child(${numDivs}n + 1)`)
 }
 
-makeDivs(256);
+makeDivs(20);
+
+const button = document.querySelector('button');
+
+function killAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+
+button.addEventListener('click', ()=> {
+
+    killAllChildNodes(container);
+
+    let input = document.querySelector('input');
+
+    let newSize = input.value;
+
+    makeDivs(newSize);
+})
+
+
+
 
